53e20064011f829260cc4ef0e570a54997a75f53
b-chae <cbr5964@naver.com>
2021-02-21 17:05:07 +0900

    Change Config (Disable Redis Caching)

--- src/main/java/com/rest/api/config/RedisConfig.java
+++ src/main/java/com/rest/api/config/RedisConfig.java
@@ -18,7 +18,7 @@ import java.util.Map;
 
 @RequiredArgsConstructor
 @EnableCaching
-@Configuration
+//@Configuration
 public class RedisConfig {
 
     @Bean(name = "cacheManager")
---
+++ src/main/java/com/rest/api/model/response/UserResponse.java
@@ -0,0 +1,2 @@
+package com.rest.api.model.response;public class UserResponse {
+}
